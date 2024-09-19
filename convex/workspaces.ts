0911103d06c6e564dc0ef6622efd0a7c11e24e61
jsmaks <jsmaks26@gmail.com>
2024-09-19 15:59:10 +0300

    add toolbar

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -21,7 +21,6 @@ export const create = mutation({
       joinCode,
     });
 
-   
     return workspaceId;
   },
 });
@@ -30,5 +29,16 @@ export const get = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query("workspaces").collect();
+  },
+});
+
+export const getById = query({
+  args: { id: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+    return await ctx.db.get(args.id);
   },
 });
--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,11 +1,15 @@
-interface WorksspaceIdPageProps {
-  params: {
-    workspaceId: string;
-  };
-}
+"use client";
 
-const WorksspaceIdPage = ({ params }: WorksspaceIdPageProps) => {
-  return <div>ID: {params.workspaceId}</div>;
+import { useWorkspaceId } from "@/app/hooks/use-workspace-id";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+
+
+
+const WorksspaceIdPage = () => {
+  const workspaceId = useWorkspaceId();
+  const { data } = useGetWorkspace({ id: workspaceId });
+  
+  return <div>Data: {JSON.stringify(data)}</div>;
 };
 
 export default WorksspaceIdPage;
