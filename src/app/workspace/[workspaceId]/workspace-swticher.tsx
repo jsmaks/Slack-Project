9694a956b5547b15a8b7a66a2182d550d130c73b
jsmaks <jsmaks26@gmail.com>
2024-09-23 23:13:19 +0300

    add  switcher

--- src/app/workspace/[workspaceId]/workspace-swticher.tsx
+++ src/app/workspace/[workspaceId]/workspace-swticher.tsx
@@ -1,5 +1,69 @@
+import { useWorkspaceId } from "@/app/hooks/use-workspace-id";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { useCreateWorkspacesModal } from "@/features/workspaces/store/use-create-workspaces-modal";
+import { Loader, Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+
 const WorkspaceSwitcher = () => {
-  return <div></div>;
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
+
+  const { data: workspaces, isLoading: workspacesLoading } = useGetWorkspaces();
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
+    id: workspaceId,
+  });
+
+  const filteredWorkspace = workspaces?.filter(
+    (workspace) => workspace?._id !== workspaceId
+  );
+
+  const [_open, setOpen] = useCreateWorkspacesModal();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button className="size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 text-slate-800 font-semibold text-xl">
+          {workspaceLoading ? (
+            <Loader className="size-5 animate-spin shrink-0" />
+          ) : (
+            workspace?.name.charAt(0).toUpperCase()
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent side="bottom" align="start" className="w-64">
+        <DropdownMenuItem
+          onClick={() => router.push(`/workspace/${workspaceId}`)}
+          className="cursor-pointer flex-col justify-start items-start capitalize">
+          {workspace?.name}
+          <span className="text-xs text-muted-foreground">
+            Active workspace
+          </span>
+        </DropdownMenuItem>
+        {filteredWorkspace?.map((workspace) => (
+          <DropdownMenuItem
+            key={workspace._id}
+            className="cursor-pointer  capitalize"
+            onClick={() =>
+              router.push(`/workspace/${workspace._id}`)
+            }></DropdownMenuItem>
+        ))}
+        <DropdownMenuItem>
+          <div className="size-9 relative overflow-hidden bg-[#F2F2F2] text-slate-800 font-semibold text-lg rounded-md flex items-center justify-center mr-2">
+            <Plus />
+            Create a new workspace
+          </div>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default WorkspaceSwitcher;
